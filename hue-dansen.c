6341031216a632fd228b4612705285520e0860d6
Will Ross <paxswill@paxswill.com>
2020-04-02 10:53:04 -0400

    First try at connecting to DTLS
    It doesn't work, but committing for record keeping.

--- hue-dansen.c
+++ hue-dansen.c
@@ -1,14 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <time.h>
+#include <arpa/inet.h>
 #include <openssl/err.h>
 #include <openssl/ssl.h>
 
-int main(int argc, char **argv)
+#define HUE_ENTERTAINMENT_PORT "2100"
+
+
+static char *identity;
+static char *psk;
+
+struct color {
+	uint16_t x;
+	uint16_t y;
+};
+
+static struct color colors[4] = {
+	// red (0.65, 0.3)
+	{42597, 19660},
+	// green (0.1, 0.7)
+	{6553, 45874},
+	// purplish-blue (0.15, 0.075)
+	{9830, 4915},
+	// orange (0.54, 0.43)
+	{35388, 28180}
+};
+
+unsigned int pskCallback(SSL *ssl,
+                         const char *hint,
+                         char *identity_buf,
+                         unsigned int max_identity_len,
+                         unsigned char *psk_buf,
+                         unsigned int max_psk_len)
 {
-	if (argc <= 4) {
-		printf("Usage:\n%s hue-IP-address identity psk\n", argv[0]);
+	strlcpy(identity_buf, identity, max_identity_len);
+	// Convert the 32 character hex string into 16 bytes
+	uint8_t pskBytes[16];
+	for (int i = 0; i < 32; i += 2) {
+		sscanf(psk + i, "%2hhX", pskBytes + i / 2);
 	}
+	memcpy(psk_buf, &pskBytes, 16);
+	psk_buf[16] = 0;
+	return 1;
 }
 
 SSL_CTX *createContext()
@@ -21,5 +59,120 @@ SSL_CTX *createContext()
 	}
 	// Force it to just DTLSv1.2
 	SSL_CTX_set_min_proto_version(ctx, DTLS1_2_VERSION);
+	// Hue entertainment only supports one cipher
+	SSL_CTX_set_cipher_list(ctx, "PSK-AES128-GCM-SHA256");
+	// Give the identity and PSK
+	SSL_CTX_set_psk_client_callback(ctx, pskCallback);
 	return ctx;
+}
+
+BIO *connectDTLS(SSL_CTX *ctx, char *host)
+{
+	BIO *bio = BIO_new_ssl_connect(ctx);
+	if (bio == NULL) {
+		fprintf(stderr, "Error connection BIO.\n");
+		ERR_print_errors_fp(stderr);
+		exit(EXIT_FAILURE);
+	}
+	BIO_set_conn_hostname(bio, host);
+	BIO_set_conn_port(bio, HUE_ENTERTAINMENT_PORT);
+
+	SSL *ssl;
+	BIO_get_ssl(bio, &ssl);
+	SSL_set_connect_state(ssl);
+	return bio;
+}
+
+// Just wrap around, we don't care.
+static uint8_t sequenceNumber;
+
+
+void setCIE(SSL *ssl,
+            uint16_t x, uint16_t y, uint16_t brightness,
+            int numLights, uint16_t *lightIDs)
+{
+	// allocate a buffer for the message.
+	// Header of 16 bytes followed by 9 bytes for each light.
+	numLights = numLights > 10 ? 10 : numLights;
+	int bufLen = 16 + numLights * 9;
+	uint8_t *buf = malloc(bufLen);
+	// Clear everything to 0
+	bzero(buf, bufLen);
+	// Write out the header.
+	// First 9 bytes are "HueStream"
+	memcpy(buf, "HueStream", 9);
+	// Next 2 are the major, then minor version (1.0)
+	buf[9] = 0x1;
+	// Next 1 byte for the sequence number
+	// (ignored for now, but sending it anyway).
+	buf[11] = sequenceNumber++;
+	// 2 Reserved bytes, set to 0
+	// 1 bytes for the color space. 0 for RGB, 1 for CIE
+	buf[14] = 1;
+	// Another 1 reserved byte, zeroed.
+	// And then the light info
+	uint8_t *lightBegin = buf + 16;
+	uint16_t *lightBuf;
+
+	for (int i = 0; i < numLights; i++) {
+		// After the first byte, everything is a 2 byte integer. The first byte
+		// is also 0 in all cases, so there's going to be an aliased buffer to
+		// make my life easier.
+		// The first byte is the device type. Lights are type 0.
+		lightBuf = (uint16_t *)(lightBegin + 1);
+		// Then 2 bytes for the light ID
+		lightBuf[0] = htons(*(lightIDs + i));
+		// Then the color value. Either RGB, or XY + brightness
+		lightBuf[1] = htons(x);
+		lightBuf[2] = htons(y);
+		lightBuf[3] = htons(brightness);
+		lightBegin += 9;
+	}
+	// Now send the entire message off to DTLS land
+	int ret = SSL_write(ssl, buf, bufLen);
+	free(buf);
+	printf("\tWrote %d bytes to DTLS\n", ret);
+	if (ret < 0) {
+		fprintf(stderr, "Error sending datagrams.\n");
+		ERR_print_errors_fp(stderr);
+		exit(EXIT_FAILURE);
+	}
+}
+
+int main(int argc, char **argv)
+{
+	if (argc != 4) {
+		fprintf(stderr, "Usage:\n%s hue-IP-address identity psk\n", argv[0]);
+		printf("Argc: %d\n", argc);
+		exit(EXIT_FAILURE);
+	}
+	identity = argv[2];
+	psk = argv[3];
+	if (strlen(psk) != 32) {
+		fprintf(stderr, "PSK needs to be exactly 32 hex digits.\n");
+		exit(EXIT_FAILURE);
+	}
+	SSL_CTX *ctx = createContext();
+	BIO *bio = connectDTLS(ctx, argv[1]);
+	SSL *ssl;
+	BIO_get_ssl(bio, &ssl);
+	SSL_connect(ssl);
+
+	int colorIndex = 0;
+	struct timespec delay;
+	delay.tv_sec = 0;
+	delay.tv_nsec = 1000000000 * 0.65;
+	// TODO: hardcoding the lights for now
+	uint16_t lightIDs[] = {0, 1};
+	while(1) {
+		struct color color = colors[colorIndex];
+		setCIE(ssl, color.x, color.y, 0xffff, 2, lightIDs);
+		if (colorIndex >= 4) {
+			colorIndex = 0;
+		} else {
+			colorIndex += 1;
+		}
+		nanosleep(&delay, NULL);
+		printf("Set color index %d\n", colorIndex);
+	}
 }
